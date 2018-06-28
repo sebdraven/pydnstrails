7d12ef6a47bc086ae4070851da1a3902eee01a0d
Sebdraven <slarinier@gmail.com>
2018-06-28 17:14:00 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pydnstrails',
+    version='1.0',
+    packages=['dnstrails'],
+    url='https://github.com/sebdraven/pydnstrails',
+    license='MIT',
+    author='Sebastien Larinier',
+    author_email='slarinier@gmail.com',
+    description=''
+)
